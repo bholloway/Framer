3e911827e2a203c7ac0227360de7c723225b8dca
Chris <chris.decoster@3plearning.com>
2015-06-18 15:37:50 +1000

    Refactor Frame to a have an options object for attributes, style and url parameters. Added doc comments, organised non prototype methods.

--- Framer.js
+++ Framer.js
@@ -2,7 +2,8 @@
 
     window.framer = {
         Framer: Framer,
-        FrameClient: FrameClient
+        FramerClient: FramerClient,
+        domLog: domLog
     };
 
     var FramerClientMessengerType = 'frame-client';
@@ -11,6 +12,7 @@
     /**
      * Framer is the manager of a set of iframe/webview elements
      * that can communicate with send and receive.
+     *
      * @param name
      * @constructor
      */
@@ -21,9 +23,26 @@
         this.focus = null;
         this.container = createFrameContainer(this.name);
         this.zIndex = 99999;
+        // The default style will make the frames fullscreen as if this is a
+        // single frame application ;)
+        this.style = {
+            position: 'fixed',
+            top: '0px',
+            left: '0px',
+            bottom: '0px',
+            right: '0px',
+            width: '100%',
+            height: '100%',
+            border: 'none',
+            margin: '0',
+            padding: '0',
+            overflow: 'hidden',
+            zIndex: '999999'
+        };
 
         this.receiveMessage = function (event) {
-            if (event.data.messenger === FramerClientMessengerType && event.data.target === this.name) {
+            if (event.data.messenger === FramerClientMessengerType &&
+                (event.data.target === this.name || typeof event.data.target === 'undefined')) {
                 this.handleMessage(event.data);
             }
         }.bind(this);
@@ -31,11 +50,23 @@
         window.addEventListener('message', this.receiveMessage, false);
     }
 
-    Framer.prototype.send = function (target, type, data) {
+    /**
+     * Send a message to a FramerClient, if no target is specified send to all.
+     * @param type the message type to use based that describes the feature, eg submit, send, logout
+     * @param data the oat
+     * @param target
+     */
+    Framer.prototype.send = function (type, data, target) {
         var message = new FrameMessage(type, data, this.name, target, FramerMessengerType);
         window.postMessage(message, document.location.origin);
     };
 
+    /**
+     * Receive a message on a Framer instance. The callback will recieve the data
+     * value sent with Framer.prototype.send()
+     * @param type
+     * @param callback
+     */
     Framer.prototype.receive = function (type, callback) {
         if (!filterByKeyValue(this.handlers, 'type', type)) {
             this.handlers.push({
@@ -47,22 +78,28 @@
         }
     };
 
-    Framer.prototype.handleMessage = function (message) {
-        var result = filterByKeyValue(this.handlers, 'type', message.type);
-        if (result) {
-            result.callback.apply(this, [message.data]);
-        }
-    };
-
-    Framer.prototype.add = function (name, src, style) {
+    /**
+     * Add a new frame to the Framer instance.
+     *
+     * @param name the unique name to manage and recieve messages
+     * @param src the src url that is given to the iframe/webview
+     * @param options an object with values for style, attributes and url arguments for the src
+     * @returns {*}
+     */
+    Framer.prototype.add = function (name, src, options) {
         var existing = filterByKeyValue(this.frames, 'name', name);
         if (existing) {
             console.warn('Framer.add() there is already a frame named',
                 name, 'in the framer', this.name);
             return existing;
         }
+        options = options || {};
 
-        var frame = new Frame(FramerClientMessengerType, name, src, style);
+        if (!options.style) {
+            options.style = this.style;
+        }
+
+        var frame = new Frame(FramerClientMessengerType, name, src, options);
         this.frames.push(frame);
 
         return frame;
@@ -70,9 +107,11 @@
 
     /**
      * Open a frame by name and close any opened frames.
+     *
      * @param name
+     * @param options
      */
-    Framer.prototype.open = function (name) {
+    Framer.prototype.open = function (name, options) {
         var existing = this.getFrame(name);
         if (!existing) {
             console.error('Framer open() there is no frame with that name', name);
@@ -83,7 +122,11 @@
             this.closeFrame(this.focus);
         }
         this.focus = existing;
-        this.openFrame(existing);
+
+        if (options) {
+            mergeOptions(this.focus.options, options);
+        }
+        this.openFrame(this.focus, options);
 
         return existing;
     };
@@ -91,29 +134,23 @@
     /**
      * Open a frame by name and instead of closing any opened frames,
      * this frame will instead open at a zIndex above the existing opened frame.
+     *
      * @param name
+     * @param options
      */
-    Framer.prototype.openAbove = function (name) {
+    Framer.prototype.openAbove = function (name, options) {
+        options = options || {};
         var existing = this.getFrame(name);
         if (!existing) {
             console.error('Framer open() there is no frame with that name', name);
             return;
         }
-        this.openFrame(existing, this.zIndex + 1);
+        if (!options.style.zIndex) {
+            options.style.zIndex = this.zIndex + 1;
+        }
+        this.openFrame(existing, options);
 
         return existing;
-    };
-
-    Framer.prototype.openFrame = function (frame, zIndex) {
-        zIndex = zIndex || this.zIndex;
-
-        if (!frame.frameElement) {
-            this.createFrameElement(frame);
-            prependElement(this.container, frame.frameElement);
-            frame.frameElement.style.zIndex = zIndex;
-        } else {
-            console.warn('Framer', this.name, 'already has', frame.name, 'open');
-        }
     };
 
     Framer.prototype.close = function (name) {
@@ -125,14 +162,39 @@
         this.closeFrame(existing);
     };
 
+    Framer.prototype.handleMessage = function (message) {
+        var result = filterByKeyValue(this.handlers, 'type', message.type);
+        if (result) {
+            result.callback.apply(this, [message.data]);
+        }
+    };
+
+    Framer.prototype.openFrame = function (frame, options) {
+        //merge existing options with override
+        if (options) {
+            mergeOptions(frame.options, options);
+        }
+
+        if (!frame.frameElement) {
+            this.createFrameElement(frame);
+            prependElement(this.container, frame.frameElement);
+
+        } else {
+            console.warn('Framer', this.name, 'already has', frame.name, 'open');
+        }
+    };
+
     Framer.prototype.closeFrame = function (frame) {
         if (frame.frameElement) {
-            // Prevent memory leaks with reload();
-            frame.frameElement.contentWindow.location.reload();
-            setTimeout(function () {
-                frame.frameElement.parentNode.removeChild(frame.frameElement);
-                frame.frameElement = undefined;
-            }, 0);
+            //todo make this configurable
+            //todo destroy event to loop through frames for custom gc clean hook
+            frame.frameElement.parentNode.removeChild(frame.frameElement);
+            frame.frameElement = undefined;
+
+            //todo Prevent memory leaks with reload();
+            //setTimeout(function () {
+            //    frame.frameElement.contentWindow.location.reload();
+            //}, 0);
         } else {
             console.warn('Framer', this.name, 'frame', frame.name, 'is not open to close');
         }
@@ -163,59 +225,40 @@
 
     Framer.prototype.createFrameElement = function (frame) {
         var src = frame.src;
-        var nameParam = '&name=' + frame.name;
+        var options = frame.options;
+        options.arguments = options.arguments || {};
+        options.style = options.style || {};
+        options.attributes = options.attributes || {};
+        var paramaters = createUrlArgs(options.arguments);
+        var params = '&name=' + frame.name + '&' + paramaters;
         var origin = '#origin=' + document.location.href;
 
-        var iframe = document.createElement('iframe'); //todo webview ms-app-webview
-        setElementStyles(iframe, frame.style);
+        //todo webview ms-app-webview
+        var iframe = document.createElement('iframe');
+
+        if (!options.style.zIndex) {
+            options.style.zIndex = this.zIndex;
+        }
+
+        setElementStyles(iframe, options.style);
+        setElementAttributes(iframe, options.attributes);
+
         iframe.id = frame.name;
-        iframe.src = src + origin + nameParam;
+        iframe.src = src + origin + params;
         frame.frameElement = iframe;
 
         return frame;
     };
 
-    //todo destroy event to loop through frames
-
-    function setElementStyles(element, styles) {
-        for (var style in styles) {
-            element.style[style] = styles[style];
-        }
-    }
-
-    function createFrameContainer(className) {
-        var container;
-        if (!elementExistsByClassName(className)) {
-            container = window.document.createElement('div');
-            container.className = className;
-            container.style.position = 'fixed';
-            container.style.top = 0;
-            container.style.left = 0;
-            container.style.zIndex = this.zIndex;
-            prependElement(window.document.body, container);
-        }
-        return container;
-    }
-
-    function prependElement(parentElement, element) {
-        return parentElement.insertBefore(element, parentElement.firstChild);
-    }
-
-    function elementExistsByClassName(className) {
-        var existingElements = window.document.getElementsByClassName(className);
-        return existingElements.length > 0;
-    }
-
-
-    function Frame(type, name, src, style) {
+    function Frame(type, name, src, options) {
         this.type = type;
         this.name = name;
         this.src = src;
-        this.style = style;
+        this.options = options;
         this.frameElement = undefined;
     }
 
-    function FrameClient() {
+    function FramerClient() {
         this.handlers = [];
 
         this.params = parseParams();
@@ -223,7 +266,8 @@
         this.origin = filterByKeyValue(this.params, 'name', 'origin').data; //todo get last value
 
         this.receiveMessage = function (event) {
-            if (event.data.messenger === FramerMessengerType && event.data.target === this.name) {
+            if (event.data.messenger === FramerMessengerType &&
+                (event.data.target === this.name || typeof event.data.target === 'undefined')) {
                 this.handleMessage(event.data);
             }
         }.bind(this);
@@ -231,19 +275,19 @@
         window.parent.window.addEventListener('message', this.receiveMessage, false);
     }
 
-    FrameClient.prototype.handleMessage = function (message) {
+    FramerClient.prototype.handleMessage = function (message) {
         var result = filterByKeyValue(this.handlers, 'type', message.type);
         if (result) {
             result.callback.apply(this, [message.data]);
         }
     };
 
-    FrameClient.prototype.send = function (target, type, data) {
+    FramerClient.prototype.send = function (type, data, target) {
         var message = new FrameMessage(type, data, this.name, target, FramerClientMessengerType);
         window.parent.postMessage(message, this.origin);
     };
 
-    FrameClient.prototype.receive = function (type, callback) {
+    FramerClient.prototype.receive = function (type, callback) {
         if (!filterByKeyValue(this.handlers, 'type', type)) {
             this.handlers.push({
                 type: type,
@@ -262,6 +306,26 @@
         this.messenger = messenger;
     }
 
+    function createUrlArgs(args) {
+        var argsList = [];
+        var properties = Object.getOwnPropertyNames(args);
+
+        properties.forEach(function propertyResolver(name) {
+            var encodedArg = name + '=' + encodeURI(args[name]);
+            argsList.push(encodedArg);
+        });
+
+        return argsList.join('&');
+    }
+
+    function mergeOptions(existing, custom) {
+        var keys = Object.getOwnPropertyNames(existing);
+        keys.forEach(function (key) {
+            if (custom[key]) {
+                existing[key] = custom[key];
+            }
+        });
+    }
 
     function filterByKeyValue(collection, key, value, last) {
         var needle;
@@ -276,7 +340,6 @@
             }
             needle = result[resultIndex];
         }
-
         return needle;
     }
 
@@ -297,6 +360,66 @@
             }
         });
         return parameters;
+    }
+
+    function prependElement(parentElement, element) {
+        return parentElement.insertBefore(element, parentElement.firstChild);
+    }
+
+    function setElementStyles(element, styles) {
+        for (var style in styles) {
+            element.style[style] = styles[style];
+        }
+    }
+
+    function setElementAttributes(element, attributes) {
+        for (var attribute in attributes) {
+            element.setAttribute(attribute, attributes[attribute]);
+        }
+        return element;
+    }
+
+    function createFrameContainer(className) {
+        var container;
+        if (!elementExistsByClassName(className)) {
+            container = window.document.createElement('div');
+            container.className = className;
+            container.style.position = 'fixed';
+            container.style.top = 0;
+            container.style.left = 0;
+            container.style.zIndex = this.zIndex;
+            prependElement(window.document.body, container);
+        }
+        return container;
+    }
+
+    function elementExistsByClassName(className) {
+        var existingElements = window.document.getElementsByClassName(className);
+        return existingElements.length > 0;
+    }
+
+    /**
+     * Simple utility to log messages to the dom.
+     * @param message
+     * @param color
+     */
+    function domLog(message, color) {
+        var className = 'dom-log';
+        var container = document.getElementsByClassName(className)[0];
+        if (!container) {
+            container = document.createElement('div');
+            container.className = className;
+            container.style.position = 'absolute';
+            container.style.top = '0';
+            container.style.right = '0';
+            prependElement(window.document.body, container);
+        }
+
+        var logMessage = document.createElement('p');
+        logMessage.innerText = message;
+        logMessage.style.color = color;
+        logMessage.style.fontSize = '0.5em';
+        container.appendChild(logMessage);
     }
 
 })(window);
