b0ad4326770697f158b4226914466cced401b779
Chris <chris.decoster@3plearning.com>
2015-06-29 14:32:40 +1000

    Add a gulp build task to separate code.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var wrap = require('gulp-wrap');
+
+var src = {
+    js: 'src/**.*js'
+};
+
+gulp.task('default', ['serve']);
+
+gulp.task('serve', ['build'], function () {
+
+    browserSync({
+        server: '.'
+    });
+
+    gulp.watch(src.js, ['build']);
+    gulp.watch(src.js).on('change', reload);
+});
+
+gulp.task('build', ['clean'], function () {
+    return gulp.src(src.js)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat('framer.min.js'))
+        .pipe(wrap('(function (window) {\n"use strict";\n<%= contents %>\n})(window);'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function (callback) {
+    del(['build'], callback);
+});
